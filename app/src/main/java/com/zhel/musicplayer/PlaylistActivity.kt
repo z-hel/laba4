a65d637d5378212084abeea051865c71348c0173
delizarov <delizarov@baccasoft.ru>
2018-11-23 09:02:50 +0400

    init

---
+++ app/src/main/java/com/zhel/musicplayer/PlaylistActivity.kt
@@ -0,0 +1,12 @@
+package com.zhel.musicplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaylistActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist)
+    }
+}
